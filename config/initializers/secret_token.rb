d8d5bfd9f76e6ea2196d935a2863df0de547d769
Mikel <constmikebtt@gmail.com>
2012-05-11 23:45:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pj1::Application.config.secret_token = 'd7e4c60247640997aec89a3dcb892844d1929e9abbeebdc1c3791d1a094ba1c939678f985e403c36e5cd249d76e0999cf5083b819cd139196aa1a59d90ad102e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pj1::Application.config.session_store :cookie_store, key: '_pj1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pj1::Application.config.session_store :active_record_store
