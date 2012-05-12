039b76a911c7eb75f3caa7058b6a338e46a4c0aa
Mikel <constmikebtt@gmail.com>
2012-05-12 15:07:27 +0300

    hihello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Pj1::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  
+
+  get "home/index"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +57,7 @@ Pj1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ db/migrate/20120512102934_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+      t.integer :superid
+      
+      t.timestamps
+    end
+  end
+end
